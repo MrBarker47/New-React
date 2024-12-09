c83e58046e6e84ffea533d027a0d5b48ff79ae5d
MrBarker47 <jebarker7@gmail.com>
2024-12-09 15:48:37 -0500

    Update files

--- React-Movie/src/App.jsx
+++ React-Movie/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default App() {
+  //Constant with your API Key
+const apiKey = "e2c8be7a";
+
+//State to hold movie data
+const [movie, setMovie] = useState(null);
+
+//Function to get movies
+const getMovie = async(searchTerm) => {
+  //Make fetch request and store response 
+  const response = await fetch(
+    `http://wwww.omdbapi.com/?apikey=${apiKey}&t${searchTerm}`
+  );
+
+  //Parse JSON response into a JavaScript object
+  const data = await response.json();
+  //Set the Movie state to the received data
+  setMovie(data)
+};
+
+return (
+    <div className='App'>
+    <Form moviesearch={getMovie} />
+    <MovieDisplay />
+    </div>
+  );
 }
-
-export default App
