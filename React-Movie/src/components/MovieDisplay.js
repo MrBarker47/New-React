9f8f7ae27c360bce17e1e653fe2a2cc547bc8fdf
MrBarker47 <jebarker7@gmail.com>
2024-12-09 16:37:29 -0500

    Update files

--- React-Movie/src/components/MovieDisplay.js
+++ React-Movie/src/components/MovieDisplay.js
@@ -1,3 +1,3 @@
-export default function MovieDisplay(props) {
-    return <h1> The MovieDisplay Component</h1>;
-};
+// export default function MovieDisplay(props) {
+//     return <h1> The MovieDisplay Component</h1>;
+// };
